b7c1a7db7b54b6ea89acc33e970fc76b2084ac3b
haroldCoder <haroldc2005@gmail.com>
2023-09-06 11:32:10 -0500

    selenium test & update chromedriver to 116 version

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\koderx\\Desktop\\javaselenium\\javaselenium\\src\\drivers\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://es.wikipedia.org/wiki/Programaci%C3%B3n");
+
+        System.out.println(driver.getTitle());
+
+        Thread.sleep(15000);
+        driver.quit();
     }
 }
